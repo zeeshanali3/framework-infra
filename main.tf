29864f11ed734a88bb50bfb834728408fb5d3672
zeeshan4859 <zeeshan4859@gmail.com>
2026-01-13 12:29:54 +0500

    update mysql version

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "azurerm_mysql_flexible_server" "mysql" {
   administrator_password = var.mysql_admin_password
 
   sku_name = "GP_Standard_D2ds_v4" # 
-  version  = "8.0"
+  version  = "8.0.21"
 
   storage {
     size_gb = 32
